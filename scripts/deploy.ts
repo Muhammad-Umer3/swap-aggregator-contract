eb103698acf36dae2092c4cbcaff48741ecc2618
Muhammad Umer <umerasif924@gmail.com>
2024-01-31 22:42:06 +0500

    feat: added deploy script for contracts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,19 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const swappingAggregator = await ethers.deployContract("SwappingAggregator");
+  await swappingAggregator.waitForDeployment();
+  console.log("Swapping Aggregator: " + await swappingAggregator.getAddress());
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const AMM = await ethers.deployContract("AMM");
+  const AMM2 = await ethers.deployContract("AMM");
 
-  await lock.waitForDeployment();
+  await Promise.allSettled([AMM.waitForDeployment(), AMM2.waitForDeployment()])
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("First AMM: " + await AMM.getAddress())
+  console.log("Second AMM: " + await AMM2.getAddress());
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
